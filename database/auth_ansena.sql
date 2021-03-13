e92e8fff3a3baf25884c6add364480897af8b141
Fachri <fachrighiffary@gmail.com>
2021-03-13 21:12:11 +0700

    upload db

--- database/auth_ansena.sql
+++ database/auth_ansena.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 10 Mar 2021 pada 11.38
+-- Generation Time: 13 Mar 2021 pada 15.07
 -- Versi Server: 10.1.16-MariaDB
 -- PHP Version: 5.5.38
 
@@ -19,18 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `auth_ansena`
 --
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `blacklist_token`
---
-
-CREATE TABLE `blacklist_token` (
-  `id` int(11) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -52,20 +40,16 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `fullname`, `email`, `phone_number`, `password`) VALUES
 (1, 'Jokowi', 'jokowi@gmail.com', '12345678912', '123123'),
-(2, 'fachri Albar', 'fachrighiffary@gmail.com', '082226068782', '$2b$04$xEHgpBIa/GWGgsimPYxzkunnBAEIPZf8.3Ck0FrwLmvsenf2n/XZy'),
+(2, 'fachri albar', 'fachrighiffary@gmail.com', '098098098098', '$2b$09$mOllwWGOnZ0qnW0q4MM2k.Tzv0yLJnOlV1UkIWs5HSnplEvWYfW9W'),
 (3, 'asadfsadfassdkfjsd', 'asdkfjsdjk@gmail.vom', '12310928', '$2b$10$BaoWhmN7BMpuDH4Ml8wdTuXAHUDiBUkeD1iMhpB/RW9mvcoHLajY2'),
 (4, 'aliando', 'aliando@gmail.com', '098123098', '$2b$04$BIgUxjqpXtgv7iIhgywcnOqAiLSeLHOWMBrauy6jHbGhGurjcjc8y'),
-(5, 'fachri', 'fachri@gmail.com', '098098123', '$2b$04$yFUyHvTDL2ntAqX7L1Hy8ekFEx6aQC/PprGzj5VyeFscBOusdboOC');
+(5, 'fachri', 'fachri@gmail.com', '098098123', '$2b$04$yFUyHvTDL2ntAqX7L1Hy8ekFEx6aQC/PprGzj5VyeFscBOusdboOC'),
+(6, 'Boy wiliam', 'alexander@gmail.com', '082226068782', '$2b$04$ZgFG.FzOjI7LATq.DTrPIuK3lxEwts1/Xb22D97Yk00R.1uZS1Dia'),
+(7, 'rivan', 'rivan@gmail.com', '123098123', '$2b$04$WXEx6gKkX9RbXjibnBAeU.T7XJuY6KjhfXgVKd7hbAceNXNBa3zQO');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `blacklist_token`
---
-ALTER TABLE `blacklist_token`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -79,15 +63,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `blacklist_token`
---
-ALTER TABLE `blacklist_token`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
